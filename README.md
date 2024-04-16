a4d803b57503176c0478b807c446c98a820c6152
Taekua <taekua@gmail.com>
2024-04-16 15:15:12 -0700

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # arduinoR4rgb-elk
 arduino program to control elk rgb lights using a voltage analog sensor
+
+
+This program is provides as is, under no circumstances I will be liable for anything that arises from using this! 
+
+This code is to be flashed into an arduino R4 with bluetooth capabilities. it uses the arduinoBLE library and
+it relies on an analog sensor I tested with voltage sensor.
+
+Ideally all you need to do at this time is find the mac address of your (ELK-BLEDOM) RGB controller and replace it on the 
+cketch file before flashing/uploading to the arduino.
+
+
+
+TODO: scan by bluetooth name and if there's only 1 device use that as MAC
---
+++ rgbVoltageContolled/rgbVoltageContolled.ino
@@ -0,0 +1,144 @@
+#include <ArduinoBLE.h>
+
+const char * mainControllerMac = "BE:16:75:00:33:1C"; //This is the address which we will connect into to turn on/off the lights
+char * bleServiceUuid = ""; //This can be hardcoded but we will try to discover it "a624bfba-50a4d532-0b4e-34d73e892943"; 
+char * targetCharacteristicUuid = "fff3";
+
+float voltageThreshold = 1.0; //Because there might be some noise, we dont want to use 0, this means when this numer of volts is
+                             // surpassed on the sensor, we will issue a command
+int STATE = 0; //1=ON 0=OFF we will use this to know if we should again issue a command, if its 1 we should notissue a power on again, and
+               //if its off, we should not try to power off again as we would prevent the user from overriding manually with their phone
+
+const uint8_t powerOnData[] = {0x7E, 0x00, 0x04, 0x00, 0x00, 0x01, 0xFF, 0x00, 0xEF}; // Hexadecimal value to write
+const uint8_t powerOffData[] = {0x7E, 0x00, 0x04, 0x00, 0x00, 0x00, 0xFF, 0x00, 0xEF}; // Hexadecimal value to write
+
+const int analogInPin = A0; //this is the pin where the analog voltage sensor should go in
+/* These variables are to store/handle sensor data*/
+float vIn;
+float oldvIn;
+float vOut;
+float voltageSensorVal;
+const float factor = 5.128;
+const float vCC = 5.00;
+/* */
+
+void setup() {
+  // the code in this function runs only once every time the arduino is powered on
+  Serial.begin(9600); 
+  pinMode(A0,INPUT); //We specify that this pin will be used for reading data  
+  Serial.begin(9600);
+  
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+    while (1);
+  } 
+  //We start looking for the rgb controller
+  BLE.scanForAddress(mainControllerMac);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // stop scanning
+    BLE.stopScan();
+    readVoltageLevel();
+
+    if (vIn > voltageThreshold) {
+      powerOn(peripheral);
+    } else {
+      powerOff(peripheral);
+    }
+    // peripheral disconnected, start scanning again
+    BLE.scanForAddress(mainControllerMac);
+  }
+  delay(1);
+}
+
+void powerOn(BLEDevice peripheral) {
+  //we dont care if its ON, since the user could have decided to override we will regardless
+  while(STATE == 0){
+    if(power(peripheral)) {
+      STATE = 1;
+    }
+  }
+}
+
+void powerOff(BLEDevice peripheral) {
+  //we dont care if its OFF, since the user could have decided to override we will regardless
+  while(STATE == 1){    
+    if(power(peripheral)) {      
+      STATE = 0;
+    }
+  }
+}
+
+
+BLECharacteristic discoveryProcess(BLEDevice peripheral) {  
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return BLECharacteristic();
+  }
+
+  for(int i=peripheral.serviceCount() -1; i>= 0; i--) {    
+    for(int j=peripheral.service(i).characteristicCount(); j >= 0; j--) {
+      //Serial.print("Char UUID =");
+      //Serial.println(peripheral.service(i).characteristic(j).uuid());
+      BLECharacteristic tempChar = peripheral.service(i).characteristic(j);
+      if (strcmp(tempChar.uuid(), targetCharacteristicUuid) == 0  && tempChar.canWrite()) {
+        //Serial.println("Found target char and its writtable");
+        return tempChar;
+      }
+    }
+  }
+  
+  Serial.println("Char discovery failed!");
+  return BLECharacteristic();
+}
+
+void readVoltageLevel() {
+  /* Read the current voltage level on the A0 analog input pin.
+     This is used here to simulate the charge level of a battery.
+  */
+  voltageSensorVal = analogRead(analogInPin);
+  vOut = (voltageSensorVal / 1024) * vCC;
+  vIn = vOut * factor;  
+}
+
+bool power(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return false;
+  }
+
+  // retrieve the LED characteristic
+  BLECharacteristic ledCharacteristic = discoveryProcess(peripheral);
+  bool written = false;
+  if (peripheral.connected()) {
+    // while the peripheral is connected
+    if(STATE == 0) {      
+      Serial.println("Powering on");
+      ledCharacteristic.writeValue(powerOnData,  sizeof(powerOnData));
+    } else {      
+      Serial.println("Powering off");
+      ledCharacteristic.writeValue(powerOffData,  sizeof(powerOffData));
+    }
+    written = true;
+  }
+  peripheral.disconnect();
+
+  Serial.println("Peripheral disconnected");
+  return written;
+}
+
+
